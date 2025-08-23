919c41037e313c93c77bbd530f0f1860d1f2f49b
shihab-63 <thecodzhub@gmail.com>
2025-08-23 17:54:52 +0600

    add js new feture

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Button Event
+const btnElment = document.getElementById('btn-login');
+btnElment.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    // fixed Number & pin
+    const mobileNumber = 11111111111;
+    const pinNumber = 1234;
+
+    // User data
+    const userNumber = document.getElementById('user-number').value;
+    const userPin = document.getElementById('user-pin').value;
+
+    // Converted NUMBER & PIN
+    const userNumberConverted = parseInt(userNumber);
+    const userPinConverted = parseInt(userPin);
+
+    // Condition Checked
+    if (userNumberConverted === mobileNumber && userPinConverted === pinNumber) {
+        confirm('Right Credential✅')
+    }else {
+        confirm('Wrong Credential❌')
+    }
+})
