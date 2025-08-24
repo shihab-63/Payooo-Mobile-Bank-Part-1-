2cf02949d64b6fad192e32194a8baf6c7907ebef
shihab-63 <thecodzhub@gmail.com>
2025-08-24 19:42:15 +0600

    add new futured

--- js/home.js
+++ js/home.js
@@ -41,18 +41,35 @@ document.getElementById('add-money-btn').addEventListener('click', function () {
     document.getElementById('cash-out-parant').style.display = 'none'
     document.getElementById('add-money-parant').style.display = 'block'
 
-    const cashOutButton = document.getElementById('cash-out-btn')
-    cashOutButton.classList.remove('bg-[#0875f25e]')
     const addMoneyButton = document.getElementById('add-money-btn')
     addMoneyButton.classList.add('bg-[#0875f25e]');
+    const cashOutButton = document.getElementById('cash-out-btn')
+    cashOutButton.classList.remove('bg-[#0875f25e]')
 
 })
 
 document.getElementById('cash-out-btn').addEventListener('click', function () {
     document.getElementById('add-money-parant').style.display = 'none'
     document.getElementById('cash-out-parant').style.display = 'block'
+
     const cashOutButton = document.getElementById('cash-out-btn')
     cashOutButton.classList.add('bg-[#0875f25e]')
     const addMoneyButton = document.getElementById('add-money-btn')
     addMoneyButton.classList.remove('bg-[#0875f25e]');
+})
+
+
+// WithDrow Section
+document.getElementById('btn-withdrow-money').addEventListener('click',function (e) {
+    e.preventDefault();
+    const amount = document.getElementById('amount-number');
+    const intAmount = parseInt(amount.value);
+
+    const availableBalance = document.getElementById('available-balance');
+    const intAvailableBalance = parseInt(availableBalance.innerText);
+    
+    // Sum 
+    const totalBalance = Math.abs(intAmount - intAvailableBalance);
+    
+    availableBalance.innerText = totalBalance;
 })
